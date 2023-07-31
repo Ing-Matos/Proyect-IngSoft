9390d2d7ab2fc54c87c9cd1d8f0d3d937d933b20
Junior Avila <20210836@itla.edu.do>
2023-07-30 20:01:53 -0400

    2.0

--- JS/app.js
+++ JS/app.js
@@ -1,3 +1,24 @@
+$(document).ready(function() {
+  // Cuando se haga scroll
+  $(window).scroll(function() {
+    // Obtenemos la posición actual del scroll
+    var scroll = $(window).scrollTop();
+
+    // Si el scroll es mayor a 100px (puedes ajustar este valor a tu preferencia)
+    if (scroll >= 100) {
+      // Agregamos la clase "fixed-top" para mantener el navbar arriba
+      $(".navbar").addClass("fixed-top");
+
+      // Agregamos la clase "navbar-scroll" para cambiar el estilo del navbar cuando haga scroll
+      $(".navbar").addClass("navbar-scroll");
+    } else {
+      // Si el scroll es menor a 100px, removemos la clase "fixed-top" y "navbar-scroll"
+      $(".navbar").removeClass("fixed-top");
+      $(".navbar").removeClass("navbar-scroll");
+    }
+  });
+});
+
 // Función para ampliar una imagen al hacer clic en ella
 function ampliarImagen(src) {
     var modal = $('<div class="modal">').css({
